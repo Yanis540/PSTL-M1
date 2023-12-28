b3910090a9fa0a2ee60cb210e135b0326945ef98
Yanis540 <yanistabellout4@gmail.com>
2023-12-28 02:26:54 +0100

    added some docs

--- utils/index.ts
+++ utils/index.ts
@@ -1,19 +1,22 @@
 /* 
 
-!   Validateur Json + typescript  :
+!   Json schema (typescript) (Validator) :
 ?       https://github.com/sagold/json-schema-library   
-!   Json Faker  :
+!   Json Faker (Generator)  :
 ?       https://github.com/json-schema-faker/json-schema-faker 
-?       https://www.npmjs.com/package/json-schema-faker)   
-
-
-
+?       (https://www.npmjs.com/package/json-schema-faker)   
 
 !   Errors : 
 ?       RequiredPropertyError
 ?       TypeError :  
 ?           -   dans le data : on trouvera le received + recieved , on peut récupérer le schéma directement 
 =>>>>>>>>>> utile pour reconstruire directement 
+
+!   Generator  :
+?       https://github.com/jimblackler/jsongenerator   
+
+
+
 
 
 */
