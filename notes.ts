acfc3397746935398974974c5fac1dcd68b06321
Yanis540 <yanistabellout4@gmail.com>
2024-01-16 00:53:51 +0100

    added some notes

---
+++ notes.ts
@@ -0,0 +1,7 @@
+/* 
+!   QuickJedi Algorithm : 
+?       extends JEDI baseline algorithm : 
+?       -   JEDI baseline : calculate min cost matching between children 
+?       -   whilst the QuickJedi CHECKS THE aggregate size bound to access whether the matching is required 
+
+*/
