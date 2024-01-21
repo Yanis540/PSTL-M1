153252d1960a37ec9dcac7466fbb02450d496db7
Yanis540 <yanistabellout4@gmail.com>
2024-01-22 00:42:20 +0100

    added notes

--- notes.ts
+++ notes.ts
@@ -4,4 +4,15 @@
 ?       -   JEDI baseline : calculate min cost matching between children 
 ?       -   whilst the QuickJedi CHECKS THE aggregate size bound to access whether the matching is required 
 
+
+
+
+!   Distance d'éditions des arbres 
+?       les opérations à faire : supprimer, modifier, ajouter des noeuds 
+?       
+
+?       à vérifier : si JEDI fait les différences après il calcule les distances, ou bien il fait les distances en premier 
+
+
+
 */
