7673149a8c658ec362061625e27e34d107e808f6
Yanis540 <yanistabellout4@gmail.com>
2024-01-29 02:56:40 +0100

    first results

--- main/scripts/expr.sh
+++ main/scripts/expr.sh
@@ -1,13 +1,1 @@
-
-echo "Experiment for persons 1 and 2"
-./ted.exe file input-data/persons/1.bracket input-data/persons/2.bracket 
-./ted.exe file input-data/persons/1.bracket input-data/persons/2.bracket  > results/persons/1-2.txt # save in the file 
-
-echo "Experiment for persons 2 and 1"
-./ted.exe file input-data/persons/2.bracket input-data/persons/1.bracket 
-./ted.exe file input-data/persons/2.bracket input-data/persons/1.bracket  > results/persons/2-1.txt # save in the file 
-
-
-
-
-
+./ted.exe file $1 $2
